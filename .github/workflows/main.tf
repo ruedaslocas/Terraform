c08b95eaef1ced9f2b3b522a97ba70d6f4ce3a60
ruedaslocas <43269502+ruedaslocas@users.noreply.github.com>
2022-07-19 01:53:46 +0200

    Add files via upload
    segunda prueba

---
+++ .github/workflows/main.tf
@@ -0,0 +1,46 @@
+# Recomendamos encarecidamente utilizar el bloque required_providers para establecer la
+# fuente y versión de Azure Provider que se está utilizando
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+  }
+
+}
+#proveedor (azure, aws, gcp...)
+provider "azurerm" {
+  features {}
+}
+
+#crear rg
+resource "azurerm_resource_group" "rg" {
+
+  #Create a resource group con variables
+  name     = var.nombre_grupo
+  location = var.localizacion_rg
+  /*
+  name     = "rg-varterraform"
+  location = "West Europe"
+  */
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name = var.nombre_sa
+  #establecemos dependencia con los valores arriba insertados
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_container" "container" {
+  #con este for each le metemos los blob desde el fichero variables
+  for_each = toset(var.containers)
+  name     = each.value
+
+  #en que SA los queremos
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "private"
+}
---
+++ .github/workflows/terraform.tfvars
@@ -0,0 +1,4 @@
+#le pasamos infomacion al variables.tf
+nombre_grupo    = "rg-terraform"
+nombre_sa       = "saterraformtwo"
+localizacion_rg = "West Europe"
